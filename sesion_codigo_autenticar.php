8dee253aa87cf9b703399de0d4cbb78e55551b97
Juan José Salazar Cedeño <36818108+scjj1986@users.noreply.github.com>
2018-03-08 10:17:56 -0400

    Update sesion_codigo_autenticar.php

--- sesion_codigo_autenticar.php
+++ sesion_codigo_autenticar.php
@@ -1,39 +1,24 @@
 <?php
 session_start();
 require("conectar.php");
-/*var_dump($_POST);
-exit;*/
 $login=strtoupper($_POST['lgn']);
 $clave=$_POST['clv'];
+$consulta=mysql_query("SELECT * FROM usuario WHERE  login='$login' and clave='$clave'") or die(mysql_error());
 
-$consulta=mysql_query("SELECT * FROM usuario WHERE  login='$login' and clave='$clave' AND activo='SI'") or die(mysql_error());
-
-if(!$resultado=mysql_fetch_array($consulta)){
-
-echo -1;
-
-}
+if(!$resultado=mysql_fetch_array($consulta))
+	echo -1;
 else{
-	$qr3=mysql_query("SELECT * FROM usuario WHERE login='$login'") or die(mysql_error());
-	$rs=mysql_fetch_array($qr3);
-	//$_SESSION['login']=$usuario;
-	$_SESSION['nombrecompleto']=$nombrecompleto=$rs['nombre']." ".$rs['apellido'];
-	//$nombrecompleto=$rs['nombre']." ".$rs['apellido'];
-	$_SESSION['perfil']=$perfil=$rs['perfil'];
-	//$_SESSION['usuario']=$usuario;
-	$_SESSION['idu']=$idus=$rs['id'];
-	if($resultado['perfil']=="ADMINISTRADOR"){
-		echo 1;
-	}
-	/*
+	$qr=mysql_query("SELECT * FROM usuario WHERE login='$login'") or die(mysql_error());
+	$rs=mysql_fetch_array($qr);
+	if ($rs["activo"]=="NO")
+		echo -2;
 	else{
-
-		echo 2;
-
-	}*/
-	
-
+		$_SESSION['nombrecompleto']=$nombrecompleto=$rs['nombre']." ".$rs['apellido'];
+		$_SESSION['perfil']=$perfil=$rs['perfil'];
+		$_SESSION['idu']=$idus=$rs['id'];
+		if($resultado['perfil']=="ADMINISTRADOR"){
+			echo 1;
+		}
+	}
 }
-
-
 ?>
